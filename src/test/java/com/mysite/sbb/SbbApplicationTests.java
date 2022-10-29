d7ed471a1ceac32eb265ff63cb218433af2611cf
iciroa <54802737+iciroa@users.noreply.github.com>
2022-10-29 22:47:48 +0900

    3-02 https://wikidocs.net/162028

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,9 @@ class SbbApplicationTests {
 
     @Autowired
     private AnswerRepository answerRepository;
+
+    @Autowired
+    private QuestionService questionService;
 
     //@Test
     void testJpa() {
@@ -126,5 +130,14 @@ class SbbApplicationTests {
         Assertions.assertThat(1).isEqualTo(answerList.size());
         Assertions.assertThat("네, 자동으로 생성됩니다.").isEqualTo(answerList.get(0).getContent());
 
+    }
+
+    @Test
+    void makeSomeData() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            questionService.create(subject, content);
+        }
     }
 }

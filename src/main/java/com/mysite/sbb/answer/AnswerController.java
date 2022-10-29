433b493073561c2fe265288b9c6b5c7f5d03f9ae
iciroa <54802737+iciroa@users.noreply.github.com>
2022-10-29 21:39:18 +0900

    2-15 https://wikidocs.net/161873 까지 진행

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -5,10 +5,12 @@ import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -18,9 +20,13 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question q = questionService.getQuestion(id);
-        answerService.create(q, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", q);
+            return "question_detail";
+        }
+        answerService.create(q, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }

55cfef53f14af0cf0544ee8d70b9140fdcbc57c1
iciroa <54802737+iciroa@users.noreply.github.com>
2022-11-02 22:07:42 +0900

    3-12 https://wikidocs.net/162763

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -40,8 +40,9 @@ public class AnswerController {
             model.addAttribute("question", q);
             return "question_detail";
         }
-        answerService.create(q, answerForm.getContent(), siteUser);
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer = answerService.create(q, answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -72,7 +73,8 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
         answerService.modify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -93,6 +95,7 @@ public class AnswerController {
         Answer answer = answerService.getAnswer(id);
         SiteUser siteUser = userService.getUser(principal.getName());
         answerService.vote(answer, siteUser);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 }
--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -14,13 +14,14 @@ import java.util.Optional;
 public class AnswerService {
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content, SiteUser siteUser) {
+    public Answer create(Question question, String content, SiteUser siteUser) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setQuestion(question);
         answer.setAuthor(siteUser);
         answer.setCreateDate(LocalDateTime.now());
         answerRepository.save(answer);
+        return answer;
     }
 
     public Answer getAnswer(Integer id) {
